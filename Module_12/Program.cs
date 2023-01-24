a660440f6044ba78eb250fe225a23be0df9253d5
Bodomrus70 <Bodomrus70@mail.ru>
2023-01-24 22:11:52 +0700

    Добавьте файлы проекта.

---
+++ Module_12/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Module_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>//список пользователей
+            {
+                new User { Login = "login23423", Name = "Mahesh Chand", IsPremium = true },
+                new User { Login = "login43535", Name = "Neel Beniwal", IsPremium = true },
+                new User { Login = "PakT", Name = "Chris Love",  IsPremium = false}
+            };
+
+
+            string login = Console.ReadLine();//ввод логина
+
+            foreach (User u in users)
+            {
+                if (u.Login == login)
+                {
+                    Console.WriteLine($"Hello, {u.Name}");//вывод приветствия с именем
+                    if (u.IsPremium == true)
+                    {
+                        User.ShowAds();
+                    }
+                }
+            }
+        }
+    }
+}
+
